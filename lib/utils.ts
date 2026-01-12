5d6afe0964377cc0b7a5c1aad0778a81140b3532
Christos TSM <christosgsd@gmail.com>
2026-01-13 00:20:36 +0200

    implement coin overview and trending coins ui and functionality using fetcher utility function

--- lib/utils.ts
+++ lib/utils.ts
@@ -2,5 +2,14 @@ import { clsx, type ClassValue } from "clsx"
 import { twMerge } from "tailwind-merge"
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+    return twMerge(clsx(inputs))
+}
+
+export function formatCurrency(value: number, currency: string = 'USD', locale: string = 'en-US'): string {
+    return new Intl.NumberFormat(locale, {
+        style: 'currency',
+        currency: currency,
+        minimumFractionDigits: 2,
+        maximumFractionDigits: 2,
+    }).format(value)
 }
